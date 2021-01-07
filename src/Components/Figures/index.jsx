4afa9faa1d25fa0e990ac6feb93ef8fe821fc09c
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2021-01-07 08:53:28 -0500

    available figures

--- src/Components/Figures/index.jsx
+++ src/Components/Figures/index.jsx
@@ -5,11 +5,66 @@ export const Figures = () => {
   return (
     <div className="figures-container">
       <h1>Lista de Items</h1>
-      <div className='looking-container'>
-        <input placeholder='Buscar...' />
+      <div className="looking-container">
+        <input placeholder="Buscar..." />
+        <button>Agregar item</button>
       </div>
       <div className="figure-container">
-        <div className="available-figures"></div>
+        <div className="available-figures">
+          <div className="title-available-figures">
+            <h1>Items disponibles</h1>
+          </div>
+          <div className="items-available-figures">
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Cuadrado</h2>
+            </div>
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Triangulo</h2>
+            </div>{" "}
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Rectangulo</h2>
+            </div>{" "}
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Rombo</h2>
+            </div>{" "}
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Pentagono</h2>
+            </div>{" "}
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Hexagono</h2>
+            </div>{" "}
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Circulo</h2>
+            </div>{" "}
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Semicirculo</h2>
+            </div>{" "}
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Estrella</h2>
+            </div>{" "}
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Anillo</h2>
+            </div>{" "}
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Cruz</h2>
+            </div>{" "}
+            <div className="item-available">
+              <img src="./assets/cuadrado.png" alt="Cuadrado" />
+              <h2>Corazón</h2>
+            </div>{" "}
+          </div>
+        </div>
         <div className="select-figures"></div>
       </div>
     </div>
