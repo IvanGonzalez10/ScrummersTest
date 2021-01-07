ad3ef4bda97a01ffb0cad77897cb4c95f9aeec55
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2021-01-07 12:03:18 -0500

    available figures resume

--- src/Components/Figures/index.jsx
+++ src/Components/Figures/index.jsx
@@ -1,13 +1,78 @@
 import React from "react";
 import "./styles.scss";
 
+const allData = [
+  {
+    name: "Cuadrado",
+    figure: "./assets/cuadrado.svg",
+  },
+  {
+    name: "Triangulo",
+    figure: "./assets/Triangulo.svg",
+  },
+  {
+    name: "Rectagulo",
+    figure: "./assets/rectangulo.svg",
+  },
+  {
+    name: "Rombo",
+    figure: "./assets/rombo.svg",
+  },
+  {
+    name: "Pentagono",
+    figure: "./assets/pentagono.svg",
+  },
+  {
+    name: "Hexagono",
+    figure: "./assets/hexagono.svg",
+  },
+  {
+    name: "Circulo",
+    figure: "./assets/circulo.svg",
+  },
+  {
+    name: "Semicirculo",
+    figure: "./assets/semicirculo.svg",
+  },
+  {
+    name: "Cuadrado",
+    figure: "./assets/anillo.svg",
+  },
+  {
+    name: "Cuadrado",
+    figure: "./assets/elipse.svg",
+  },
+  {
+    name: "Estrella",
+    figure: "./assets/estrella.svg",
+  },
+  {
+    name: "Cruz",
+    figure: "./assets/cruz.svg",
+  },
+];
+
 export const Figures = () => {
+  const [activeData, setActiveData] = React.useState(0);
+  const maxData = allData.length;
+
+  const addItem = () => {
+    setActiveData((itemsActiveData) => itemsActiveData + 1);
+  };
+
+  const removeItem = () => {
+    setActiveData((itemsActiveData) => itemsActiveData - 1);
+  };
+
+  const reloadItem = (data) => {
+    setActiveData(data);
+  };
   return (
     <div className="figures-container">
       <h1>Lista de Items</h1>
       <div className="looking-container">
         <input placeholder="Buscar..." />
-        <button>Agregar item</button>
+        <button onClick={addItem}>Agregar item</button>
       </div>
       <div className="figure-container">
         <div className="available-figures">
@@ -15,57 +80,22 @@ export const Figures = () => {
             <h1>Items disponibles</h1>
           </div>
           <div className="items-available-figures">
+            {allData.map((data, index) => (
             <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Cuadrado</h2>
+              <img src={data.figure} alt={data.name} />
+              <h2>{data.name}</h2>
             </div>
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Triangulo</h2>
-            </div>{" "}
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Rectangulo</h2>
-            </div>{" "}
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Rombo</h2>
-            </div>{" "}
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Pentagono</h2>
-            </div>{" "}
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Hexagono</h2>
-            </div>{" "}
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Circulo</h2>
-            </div>{" "}
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Semicirculo</h2>
-            </div>{" "}
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Estrella</h2>
-            </div>{" "}
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Anillo</h2>
-            </div>{" "}
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Cruz</h2>
-            </div>{" "}
-            <div className="item-available">
-              <img src="./assets/cuadrado.png" alt="Cuadrado" />
-              <h2>Corazón</h2>
-            </div>{" "}
+            ))}
           </div>
         </div>
-        <div className="select-figures"></div>
+        <div className="select-figures">
+          <div className="title-select-figures">
+            <h1>Figuras agregadas</h1>
+          </div>
+          <div className="added-figures">
+            <img src={allData.figure} alt={allData.name} />
+          </div>
+        </div>
       </div>
     </div>
   );
