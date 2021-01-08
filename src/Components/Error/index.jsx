ddc42758578a60d00f12902d1f8a52bf4037c50d
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2021-01-07 22:04:58 -0500

    changing not found styles

--- src/Components/Error/index.jsx
+++ src/Components/Error/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles.scss";
+import { Link } from "@reach/router";
 
 export const Error = () => {
   return (
     <div className='error-container'>
       <h1>Scrummers</h1>
       <h2>404 Error</h2>
-      <h3>La URL solicitada no existe</h3>
+      <h3>La URL solicitada no existe. Por favor vuelve al Inicio.</h3>
+      <Link to="/"><button>Inicio</button></Link>
     </div>
   );
 };
